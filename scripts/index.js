b35386601b368a07035e3bc10aaffb5b266fe317
Ww228wW <andobatay@gmail.com>
2022-11-17 19:27:56 +0300

    Уже запутался

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,28 @@
 let ButtonEdit = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let editlock = document.querySelector('.popup__container-close');
-ButtonEdit.onclick = function() {
-popup.classList.toggle('popup__opened'); 
-}
-editlock.onclick = function() { 
-popup.classList.toggle('popup__opened');
+ButtonEdit.addEventListener('click', function () {
+  popup.classList.toggle('popup_opened')
+})
+editlock.addEventListener('click', function () {
+  popup.classList.toggle('popup_opened'  )
+})
+// ButtonEdit.onclick = function() {popup.classList.add('popup_opened')}; Этот спопсоб короче , но в брифе по другому 
+// editlock.onclick = function() {popup.classList.remove('popup_opened');}
 window.addEventListener('click', e => {   
- 
 // при клике в любом месте окна браузера
    const target = e.target // находим элемент, на котором был клик
    if (!target.closest('.popup__container') && !target.closest('.profile__edit-button')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-     popup.classList.remove('popup__opened') // то закрываем окно навигации, удаляя активный класс , после будет добавлена фунуция сохранения при закрытии без кнопки 
-    // Перенос при закрытии popup
-let proName = document.querySelector('.profile__name').textContent;
-document.querySelector('.popup__form-input_name').value = proName;
-let proBio = document.querySelector('.profile__bio').textContent;
-document.querySelector('.popup__form-input_bio').value = proBio;
-this}
+     popup.classList.remove('popup_opened')  // то закрываем окно навигации, удаляя активный класс , после будет добавлена фунуция сохранения при закрытии без кнопки 
+    
+     // данные profile при закрытии popup
+    let proName = document.querySelector('.profile__name').textContent;
+    document.querySelector('.popup__form-input_name').value = proName;
+    let proBio = document.querySelector('.profile__bio').textContent;
+    document.querySelector('.popup__form-input_bio').value = proBio;
+}
  })
 
-}
 // Редактирование профиля
 let formElement = document.querySelector ('.popup__form');
 // Находим поля формы в DOM
@@ -38,7 +40,9 @@ function formSubmitHandler (evt) {
     // Вставьте новые значения с помощью textContent
     name.textContent = nameInput;
     bio.textContent = jobInput;
+    popup.classList.remove('popup_opened');
 }
+
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
@@ -48,15 +52,4 @@ let proName = document.querySelector('.profile__name').textContent;
 document.querySelector('.popup__form-input_name').value = proName;
 let proBio = document.querySelector('.profile__bio').textContent;
 document.querySelector('.popup__form-input_bio').value = proBio;
-// Cо слезами на глазах доделал к 00:00 сидел с 15:00, пишу не чтобы поплакать , а чтобы возгордится  :)
-
-
-// // Открыть Блок (Неудачный вариант, пускай будет мне не мешает) 
-// let ButtonProfile = document.querySelector('.profile__add-button');
-// let emptylock = document.querySelector('.popup__empty');
-// let emptylocks = document.querySelector('.popup__close_btn');
-// ButtonProfile.onclick = function() {
-// emptylock.classList.add('popup__opened'); 
-// }
-// emptylocks.onclick = function() {
-// emptylock.classList.remove('popup__opened');
+// Cо слезами на глазах доделал к 00:00 сидел с 15:00, пишу не чтобы поплакать , а чтобы возгордится  :) x2
