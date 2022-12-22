b284f6efa5eba3258182c19be787c7c8370f4696
Ww228wW <89904382+Ww228wW@users.noreply.github.com>
2022-12-22 13:22:39 +0300

    6 проект ветка

--- scripts/index.js
+++ scripts/index.js
@@ -28,75 +28,68 @@ const popImage = popPhoto.querySelector(".popup__image")
 const popImageText = popPhoto.querySelector(".popup__photo-text")
 
 // Массив 
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-]
+const initialCards = [{
+	name: "Архыз",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+}, {
+	name: "Челябинская область",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+}, {
+	name: "Иваново",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+}, {
+	name: "Камчатка",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+}, {
+	name: "Холмогорский район",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+}, {
+	name: "Байкал",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+}, ]
 const ESC = 'Escape';
 const closePopupEsc = (evt) => {
-  if (evt.key === ESC) {
-    closePopup(document.querySelector(`.popup_opened`));
-  }
+	if (evt.key === ESC) {
+		closePopup(document.querySelector(`.popup_opened`));
+	}
 };
 document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("popup")) {
-    closePopup(e.target);
-  }
+	if (e.target.classList.contains("popup")) {
+		closePopup(e.target);
+	}
 });
 
 popAddBtn.addEventListener('click', function() {
-  openPopup(popAdd);
+	openPopup(popAdd);
 });
 
 function openPopup(popup) {
-  document.addEventListener('keydown', closePopupEsc);
-  popup.classList.add('popup_opened');
-} 
+	document.addEventListener('keydown', closePopupEsc);
+	popup.classList.add('popup_opened');
+}
 
 function closePopup(popup) {
-  document.removeEventListener('keydown', closePopupEsc);
-  popup.classList.remove('popup_opened');
+	document.removeEventListener('keydown', closePopupEsc);
+	popup.classList.remove('popup_opened');
 }
 const popClose = (evt) => {
-    const screen = evt.target.classList.contains("popup");
-    const close = evt.target.classList.contains("popup__container-close");
-    if (screen || close) {
-      closePopup();
-    }
+	const screen = evt.target.classList.contains("popup");
+	const close = evt.target.classList.contains("popup__container-close");
+	if (screen || close) {
+		closePopup();
+	}
 };
 
 closePopButtons.forEach(closeButton => {
-  const popup = closeButton.closest('.popup');
-  closeButton.addEventListener('click', () => closePopup(popup));
+	const popup = closeButton.closest('.popup');
+	closeButton.addEventListener('click', () => closePopup(popup));
 });
 
-function handleFormSubmit (evt) {
-   evt.preventDefault();
-   profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-   closePopup(popEdit);
+function handleFormSubmit(evt) {
+	evt.preventDefault();
+	profileName.textContent = nameInput.value;
+	profileJob.textContent = jobInput.value;
+	closePopup(popEdit);
 }
 // Сохранение popup_edit
 popEditBtn.addEventListener("click", function() {
@@ -112,7 +105,7 @@ function handleProfileFormSubmit(evt) {
 	closePopup(popEdit)
 }
 PopEditForm.addEventListener("submit", handleProfileFormSubmit)
- 
+
 // Генерация карточек
 function createElement(src, alt) {
 	const template = document.querySelector("#element").content
@@ -121,13 +114,17 @@ function createElement(src, alt) {
 	elemImage.setAttribute("src", src)
 	elemImage.setAttribute("alt", alt)
 	element.querySelector(".element__title").textContent = alt
-	// element.querySelector(".element__like").addEventListener("click", function(like) {like.target.classList.toggle("element__like-active")})
-  element.addEventListener('click', function (evt) { 
-    if (evt.target.classList.contains('element__like')) {
-      evt.target.classList.toggle('element__like-active')}});  
-	
-  // element.addEventListener('click', function (evt) { if (evt.target.classList.contains('element__delete')) {evt.target.closest('.element').remove}});  
-  element.querySelector(".element__delete").addEventListener("click", function(del) {del.target.closest(".element").remove()})
+		// element.querySelector(".element__like").addEventListener("click", function(like) {like.target.classList.toggle("element__like-active")})
+	element.addEventListener('click', function(evt) {
+		if (evt.target.classList.contains('element__like')) {
+			evt.target.classList.toggle('element__like-active')
+		}
+	});
+
+	// element.addEventListener('click', function (evt) { if (evt.target.classList.contains('element__delete')) {evt.target.closest('.element').remove}});  
+	element.querySelector(".element__delete").addEventListener("click", function(del) {
+		del.target.closest(".element").remove()
+	})
 	elemImage.addEventListener("click", function(event) {
 		openImagePop(src, alt)
 	})
@@ -140,7 +137,7 @@ function openImagePop(image, text) {
 	popImage.setAttribute("src", image)
 	popImageText.textContent = text
 	popImage.setAttribute("alt", text)
-} 
+}
 
 function addElem(src, alt) {
 	const card = createElement(src, alt)
