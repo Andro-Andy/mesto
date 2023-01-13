cc25176a3beba91f25b43eb5297a139b1c43195a
Ww228wW <89904382+Ww228wW@users.noreply.github.com>
2023-01-13 18:19:06 +0300

    7 Спринт

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,68 +1,63 @@
 export default class FormValidator {
+
 	constructor(enableValidation, form) {
-		this._enableValidation = enableValidation
-		this._form = form
-		this._inputs = Array.from(
-			this._form.querySelectorAll(this._enableValidation.inputSelector)
-		)
-		this._buttonSave = this._form.querySelector(
-			this._enableValidation.submitButtonSelector
-		)
+		this._enableValidation = enableValidation;
+		this._form = form;
+		this._inputs = Array.from(this._form.querySelectorAll(this._enableValidation.inputSelector));
+		this._buttonSave = this._form.querySelector(this._enableValidation.submitButtonSelector);
 	}
 
-	_inputValidity(input) {
+	_checkInputValidity(input) {
 		if (!input.validity.valid) {
-			this._inputError(input)
+			this._showInputError(input);
 		} else {
-			this._hideInputError(input)
+			this._hideInputError(input);
 		}
 	}
-	_inputError(input) {
-		const error = this._form.querySelector(`#${input.id}-error`)
-		error.classList.add(this._enableValidation.errorClass)
-		input.classList.add(this._enableValidation.inputErrorClass)
-		error.textContent = input.validationMessage
+	_showInputError(input) {
+		const error = this._form.querySelector(`#${input.id}-error`);
+		input.classList.add(this._enableValidation.inputErrorClass);
+		error.classList.add(this._enableValidation.errorClass);
+		error.textContent = input.validationMessage;
 	}
 	_hideInputError(input) {
-		const error = this._form.querySelector(`#${input.id}-error`)
-		input.classList.remove(this._enableValidation.inputErrorClass)
-		error.classList.remove(this._enableValidation.errorClass)
-		error.textContent = ""
+		const error = this._form.querySelector(`#${input.id}-error`);
+		input.classList.remove(this._enableValidation.inputErrorClass);
+		error.classList.remove(this._enableValidation.errorClass);
+		error.textContent = '';
 	}
-	_toggleButton() {
-		const isFormValid = Array.from(this._inputs).every((input) => {
-			return input.validity.valid
-		})
+	_toggleButtonState() {
+		const isFormValid = Array.from(this._inputs).every(input => {
+			return input.validity.valid;
+		});
 
 		if (isFormValid) {
-			this._buttonSave.classList.remove(
-				this._enableValidation.inactiveButtonClass
-			)
-			this._buttonSave.removeAttribute("disabled")
+			this._buttonSave.classList.remove(this._enableValidation.inactiveButtonClass);
+			this._buttonSave.removeAttribute('disabled');
 		} else {
-			this._buttonSave.classList.add(this._enableValidation.inactiveButtonClass)
-			this._buttonSave.setAttribute("disabled", true)
+			this._buttonSave.classList.add(this._enableValidation.inactiveButtonClass);
+			this._buttonSave.setAttribute('disabled', true);
 		}
 	}
 	_setEventListeners() {
-		this._toggleButton()
-		this._inputs.forEach((input) => {
-			input.addEventListener("input", () => {
-				this._inputValidity(input)
-				this._toggleButton()
-			})
-		})
-	}
+		this._toggleButtonState();
 
+
+		this._inputs.forEach(input => {
+			input.addEventListener('input', () => {
+				this._checkInputValidity(input);
+				this._toggleButtonState();
+			});
+		});
+	}
 	enableValidation() {
-		this._setEventListeners()
-	}
-
+		this._setEventListeners();
+	};
 	resetFormErrors() {
-		this._form.reset()
+		this._form.reset();
 		this._inputs.forEach((input) => {
-			this._hideInputError(input)
+			this._hideInputError(input);
 		})
-		this._toggleButton()
+		this._toggleButtonState();
 	}
-}
+};
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import Card from "./Card.js"
-import FormValidator from "./FormValidator.js"
+import Card from "./Card.js";
+import FormValidator from './FormValidator.js';
 // Попапы
 const popEdit = document.querySelector(".popup_edit")
 const popAdd = document.querySelector(".popup_add")
@@ -9,13 +9,13 @@ const popPhoto = document.querySelector(".popup_photo")
 const popEditBtn = document.querySelector(".profile__edit-button")
 const popAddBtn = document.querySelector(".profile__add-button")
 const closePopButtons = document.querySelectorAll(".popup__container-close")
-const KEYESC = "Escape"
+const KEYESC = 'Escape';
 
 // Формы
 const elems = document.querySelector(".elements")
 const PopEditForm = document.forms["profile-form"]
 const PopAddForm = document.forms["element-form"]
-const formAdd = popAdd.querySelector(".popup__form")
+const formAdd = popAdd.querySelector('.popup__form');
 
 // Изменение значений
 const inpName = document.querySelector(".popup__form-input_name")
@@ -31,114 +31,108 @@ const popAddLink = popAdd.querySelector(".popup__form-input_link")
 const popImage = popPhoto.querySelector(".popup__image")
 const popImageText = popPhoto.querySelector(".popup__photo-text")
 
+
 const enableValidation = {
-	formSelector: ".popup__form",
-	inputSelector: ".popup__form-input",
-	submitButtonSelector: ".popup__form-submit",
-	inactiveButtonClass: "popup__form-submit_invalid",
-	inputErrorClass: "popup__form-input_error",
-	errorClass: "popup__error_visibility",
-}
+	formSelector: '.popup__form',
+	inputSelector: '.popup__form-input',
+	submitButtonSelector: '.popup__form-submit',
+	inactiveButtonClass: 'popup__form-submit_invalid',
+	inputErrorClass: 'popup__form-input_error',
+	errorClass: 'popup__error_visibility'
+};
 
 // Массив
-const initialCards = [
-	{
-		name: "Архыз",
-		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-	},
-	{
-		name: "Челябинская область",
-		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-	},
-	{
-		name: "Иваново",
-		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-	},
-	{
-		name: "Камчатка",
-		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-	},
-	{
-		name: "Холмогорский район",
-		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-	},
-	{
-		name: "Байкал",
-		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-	},
-]
+const initialCards = [{
+	name: "Архыз",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+}, {
+	name: "Челябинская область",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+}, {
+	name: "Иваново",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+}, {
+	name: "Камчатка",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+}, {
+	name: "Холмогорский район",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+}, {
+	name: "Байкал",
+	link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+},]
 
-const validationPopPro = new FormValidator(enableValidation, popEdit)
-validationPopPro.enableValidation()
 
-const validationPopAdd = new FormValidator(enableValidation, formAdd)
-validationPopAdd.enableValidation()
+const validationPopupProfile = new FormValidator(enableValidation, popEdit);
+validationPopupProfile.enableValidation();
 
-const closePopEsc = (evt) => {
+const validationPopupAdd = new FormValidator(enableValidation, formAdd);
+validationPopupAdd.enableValidation();
+
+const closePopupEsc = (evt) => {
 	if (evt.key === KEYESC) {
-		closePopup(document.querySelector(`.popup_opened`))
+		closePopup(document.querySelector(`.popup_opened`));
 	}
-}
+};
 document.addEventListener("click", (e) => {
 	if (e.target.classList.contains("popup")) {
-		closePopup(e.target)
+		closePopup(e.target);
 	}
-})
-function closePopup(popup) {
-	document.removeEventListener("keydown", closePopEsc)
-	popup.classList.remove("popup_opened")
-}
+});
 function openPopup(popup) {
-	document.addEventListener("keydown", closePopEsc)
-	popup.classList.add("popup_opened")
+	document.addEventListener("keydown", closePopupEsc);
+	popup.classList.add("popup_opened");
 }
-
+function closePopup(popup) {
+	document.removeEventListener("keydown", closePopupEsc);
+	popup.classList.remove("popup_opened");
+}
 closePopButtons.forEach((closeButton) => {
-	const popup = closeButton.closest(".popup")
-	closeButton.addEventListener("click", () => closePopup(popup))
-})
+	const popup = closeButton.closest(".popup");
+	closeButton.addEventListener("click", () => closePopup(popup));
+});
 
-initialCards.forEach(render)
+initialCards.forEach(render);
 
 popEditBtn.addEventListener("click", function () {
-	openPopup(popEdit)
-	inpName.value = proName.textContent
-	inpBio.value = proBio.textContent
-})
+	openPopup(popEdit);
+	inpName.value = proName.textContent;
+	inpBio.value = proBio.textContent;
+});
 
 function handleImageClick(data) {
-	popImageText.textContent = data.name
-	popImage.src = data.link
-	popImage.alt = data.name
-	openPopup(popPhoto)
+	popImageText.textContent = data.name;
+	popImage.src = data.link;
+	popImage.alt = data.name;
+	openPopup(popPhoto);
 }
 
 function render(data) {
-	const item = new Card(data, "#element", handleImageClick)
-	const newItem = item.createElement()
-	elems.prepend(newItem)
+	const item = new Card(data, "#element", handleImageClick);
+	const newItem = item.createElement();
+	elems.prepend(newItem);
 }
 
 popAddBtn.addEventListener("click", () => {
-	validationPopAdd.resetFormErrors()
-	openPopup(popAdd)
-})
+	validationPopupAdd.resetFormErrors();
+	openPopup(popAdd);
+});
 PopEditForm.addEventListener("submit", function submitformHandler(evt) {
-	evt.preventDefault()
-	proName.textContent = inpName.value
-	proBio.textContent = inpBio.value
-	closePopup(popEdit)
-})
+	evt.preventDefault();
+	proName.textContent = inpName.value;
+	proBio.textContent = inpBio.value;
+	closePopup(popEdit);
+});
 
 PopAddForm.addEventListener("submit", function submitformHandler(evt) {
-	evt.preventDefault()
+	evt.preventDefault();
 
 	const newcard = {
 		name: popAddTitle.value,
 		link: popAddLink.value,
-	}
+	};
 
-	render(newcard)
-	closePopup(popAdd)
-	PopAddForm.reset()
-})
+	render(newcard);
+	closePopup(popAdd);
+	PopAddForm.reset();
+});
